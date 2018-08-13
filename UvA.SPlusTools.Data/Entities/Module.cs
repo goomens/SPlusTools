15787e7446ac29e82eedd6d75b3a31a972cdbf3d
Gerrit Oomens <g.oomens@uva.nl>
2018-08-13 23:39:13 +0200

    Get activities per module

---
+++ UvA.SPlusTools.Data/Entities/Module.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UvA.SPlusTools.Data.Entities
+{
+    partial class Module
+    {
+        public SPlusCollection<Activity> GetActivities()
+            => new SPlusCollection<Activity>(College, Object.ActivitiesAllocatedTo());
+    }
+}
