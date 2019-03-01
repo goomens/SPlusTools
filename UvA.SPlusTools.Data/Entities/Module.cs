49ef5c7c5a9c401b6da7b643f7b3592334ee62c9
Gerrit Oomens <g.oomens@uva.nl>
2019-03-01 16:50:15 +0100

    Get linked activities per module

--- UvA.SPlusTools.Data/Entities/Module.cs
+++ UvA.SPlusTools.Data/Entities/Module.cs
@@ -8,7 +8,10 @@ namespace UvA.SPlusTools.Data.Entities
 {
     partial class Module
     {
-        public SPlusCollection<Activity> GetActivities()
+        public SPlusCollection<Activity> GetAllocatedActivities()
             => new SPlusCollection<Activity>(College, Object.ActivitiesAllocatedTo());
+
+        public SPlusCollection<Activity> GetLinkedActivities()
+            => new SPlusCollection<Activity>(College, Object.ActivitiesLinkedTo());
     }
 }
